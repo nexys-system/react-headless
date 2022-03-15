086c420342e583ea52b27894ab3a58f8d1d137a5
Johan <johan@macair2020>
2022-03-15 14:10:48 +0100

    added layout

--- src/lib/form/utils.ts
+++ src/lib/form/utils.ts
@@ -2,20 +2,8 @@ export const enumToOptions = <A>(keys: {
   [s: number]: string;
 }): { id: A; name: string }[] =>
   Object.keys(keys)
-    .filter((x) => !isNaN(Number(x)))
-    .map((x) => ({ id: Number(x) as any as A, name: keys[Number(x)] }));
-
-export const getClassName = (errors?: string[]): string => {
-  const isInvalid: boolean = !!errors;
-
-  const classes = ["form-control"]; //
-
-  if (isInvalid) {
-    classes.push("is-invalid");
-  }
-
-  return classes.join(" ");
-};
+    .filter(x => !isNaN(Number(x)))
+    .map(x => ({ id: Number(x) as any as A, name: keys[Number(x)] }));
 
 export const isA = <A>(
   a: Partial<A>,
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,9 +5,12 @@ import { Switch, Route } from 'react-router-dom';
 import Public from './public';
 import Layout from './layout/index';
 
+import L3 from './components/layout';
+
 const Routes = (): JSX.Element => (
   <Layout>
     <Switch>
+      <Route path={'/layout'} component={L3} />
       <Route component={Public} />
     </Switch>
   </Layout>
