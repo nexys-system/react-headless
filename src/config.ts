2053bce3b6695089542a12462233635fd07e11e8
John B <johan@nexys.ch>
2022-03-23 10:02:31 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-
-export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
+export const version = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_version";
+export const sha = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_sha";
 export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "Core-List";
 export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
 export const ghUrl = "https://github.com/nexys-system/core-list";
 
-export const github = { sha: `${ghUrl}/commit/${sha}`, url: ghUrl };
+export const github = { sha: `${ghUrl}/commit/${sha}`, version: `${ghUrl}/releases/tag/${version}`, url: ghUrl };
