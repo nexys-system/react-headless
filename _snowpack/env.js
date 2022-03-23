72a77377feddfd72046b7443ea8744eca20bf40e
johnb8005 <johnb8005@users.noreply.github.com>
2022-03-23 08:27:00 +0000

    deploy: 0dde233440901e4344795cd9e4a45aef8a1c4c49

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "7a4724a16202fe3fcb6f6127bb122b9a08705034";
+export const SNOWPACK_PUBLIC_GIT_SHA = "0dde233440901e4344795cd9e4a45aef8a1c4c49";
 export const SNOWPACK_PUBLIC_URL = "/core-list";
 export const MODE = "production";
 export const NODE_ENV = "production";
