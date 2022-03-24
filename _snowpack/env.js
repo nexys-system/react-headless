0bed6551bfc413b2c0878dd2f2ad3ef12d86df68
johnb8005 <johnb8005@users.noreply.github.com>
2022-03-24 10:02:59 +0000

    deploy: c65419fafe7a34e28f5c3be598e86d0751a34429

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "0a7e80c30dad72fdbde7adb8d0427197c8979d84";
+export const SNOWPACK_PUBLIC_GIT_SHA = "c65419fafe7a34e28f5c3be598e86d0751a34429";
 export const SNOWPACK_PUBLIC_URL = "/react-headless";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
 
-export const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "unset_version";
-export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unset_sha";
+export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "unset_version";
+export const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unset_sha";
 export const title = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_TITLE || "react-headless";
 export const basename = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_URL;
 export const ghUrl = "https://github.com/nexys-system/core-list";
