01236e1e927e27010a56b82b6e562e2b26f8cc43
John B <johan@nexys.ch>
2022-03-24 11:06:20 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_version";
-export const version = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_sha";
+export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
+export const version = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
 export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "react-headless";
 export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
 export const ghUrl = "https://github.com/nexys-system/core-list";
