0569934c7954d8aa1371a3ba18ca47169d15eee2
johnb8005 <johnb8005@users.noreply.github.com>
2022-03-24 10:13:24 +0000

    deploy: ffea1eff34e27678b03062c84b3b9c6798bc7840

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "4d02b3d620b037de22adaa17cd30bb8b24e212f4";
+export const SNOWPACK_PUBLIC_GIT_SHA = "ffea1eff34e27678b03062c84b3b9c6798bc7840";
 export const SNOWPACK_PUBLIC_VERSION = "master";
 export const SNOWPACK_PUBLIC_URL = "/react-headless";
 export const MODE = "production";
