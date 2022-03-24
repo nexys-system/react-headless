a4e00282178241db476a4bf0278e7547cae08fc5
johnb8005 <johnb8005@users.noreply.github.com>
2022-03-24 12:13:56 +0000

    deploy: 2775654406c3a4343f1d08f138b51c6a00e0fc20

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "906afd764c37c588ffb15524b832a8d7355b2b8e";
-export const SNOWPACK_PUBLIC_VERSION = "v0.1.11";
+export const SNOWPACK_PUBLIC_GIT_SHA = "2775654406c3a4343f1d08f138b51c6a00e0fc20";
+export const SNOWPACK_PUBLIC_VERSION = "v0.1.12";
 export const SNOWPACK_PUBLIC_URL = "/react-headless";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- examples/form/index.js
+++ examples/form/index.js
@@ -3,10 +3,16 @@ import FormWrapper from "../../lib/form/form-wrapper.js";
 import {delay} from "../utils.js";
 import FormUI from "./ui.js";
 const shape = {firstName: {}, lastName: {optional: true}};
-const apiCall = async () => {
+const cartoonCharacters = ["mickey", "minnie", "donald", "popeye"];
+const apiCall = async (data) => {
   await delay();
+  if (cartoonCharacters.includes(data.firstName.toLowerCase())) {
+    return Promise.reject({
+      firstName: ["Cartoon character names are not allowed"]
+    });
+  }
   return {id: 2};
 };
 const onSuccess = (a, b) => alert("form sent successfully" + b.id + " " + JSON.stringify(a));
-const Form = FormWrapper(FormUI, shape, apiCall, onSuccess);
-export default () => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Form"), /* @__PURE__ */ React.createElement(Form, null));
+const Form = FormWrapper(FormUI, shape, apiCall, onSuccess, (errors) => ({errors}));
+export default () => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Form"), /* @__PURE__ */ React.createElement("p", null, "Form demo.", " ", /* @__PURE__ */ React.createElement("small", null, "To simulate a form rejection insert one of the following first names:", " ", /* @__PURE__ */ React.createElement("code", null, JSON.stringify(cartoonCharacters)))), /* @__PURE__ */ React.createElement(Form, null));
