6b608e1f8c828c23fd8c352f9ff367cca13ff256
Johan <johan@macair2020>
2022-04-01 11:44:20 +0200

    add download

---
+++ src/examples/downloads.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { saveByteArray } from '../lib/utils';
+
+const downloadTxt = () => {
+  saveByteArray('hellow world', 'out.txt');
+};
+
+const downloadCsv = () => {
+  const csv = [
+    ['col 1', 'col 2', 'col 3'],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
+  const csvString = csv.map(row => row.join(',')).join('\n');
+
+  saveByteArray(csvString, 'out.csv', 'text/csv');
+};
+
+export default () => {
+  return (
+    <>
+      <h1>Download</h1>
+
+      <ul>
+        <li>
+          <button
+            className="btn btn-outline-primary btn-sm"
+            onClick={downloadTxt}
+          >
+            Donwload txt
+          </button>
+        </li>
+        <li>
+          <button
+            className="btn btn-outline-primary btn-sm"
+            onClick={downloadCsv}
+          >
+            Donwload CSV
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+};
