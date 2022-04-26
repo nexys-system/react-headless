9022e5a742f1c75cc0bbe8902490f02b661196b7
johnb8005 <johnb8005@users.noreply.github.com>
2022-04-26 13:47:31 +0000

    deploy: 3377b0a27688a08fe26f427427869d442a1da70a

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "b9f16bc5dc880f62bbde3b3b05199fbc4a926d6f";
-export const SNOWPACK_PUBLIC_VERSION = "v0.5.0";
+export const SNOWPACK_PUBLIC_GIT_SHA = "3377b0a27688a08fe26f427427869d442a1da70a";
+export const SNOWPACK_PUBLIC_VERSION = "v0.5.1";
 export const SNOWPACK_PUBLIC_URL = "/react-headless";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- examples/form/generated.js
+++ examples/form/generated.js
@@ -1,13 +1,8 @@
-import React from "../../_snowpack/pkg/react.js";
-import FormUIGenerate from "../../lib/form/ui-generator.js";
-import * as UI from "../../components/form/inputs.js";
 import {FormUIType} from "../../lib/form/type.js";
+import PreForm from "../../components/form/generator.js";
+import {apiCall} from "./utils.js";
 const def = [
   {name: "firstName", uiType: FormUIType.Text, optional: false},
   {name: "lastName", uiType: FormUIType.Text, optional: true}
 ];
-const FormUIGenerated = FormUIGenerate(UI.InputWrapper, UI.InputGeneric, () => /* @__PURE__ */ React.createElement("button", {
-  className: "btn btn-primary",
-  type: "submit"
-}, "Send"))(def);
-export default FormUIGenerated;
+export default PreForm(def, apiCall);
