5c936d9d237c8ae1929b42a80d797ac09f9a2f86
Johan <johan@macair2020>
2022-05-18 01:48:45 +0200

    change env var

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
-export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
-export const version =
-  import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
+export const sha = import.meta.env.VITE_GIT_SHA || "unset_sha";
+export const version = import.meta.env.VITE_VERSION || "unset_version";
 export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "react-headless";
 export const basename = import.meta.env.VITE_BASE || "";
 export const ghUrl = "https://github.com/nexys-system/react-headless";
