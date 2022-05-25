32d38ae5e5ed4b2fa30934a59ee8f46c9dff506c
Johan <johan@macair2020>
2022-05-25 11:27:29 +0200

    fix tests

---
+++ src/lib/table/index.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+import * as Index from "./index";
+
+test("imports", () => {
+  //expect(typeof Index.List).toEqual('function');
+  expect(typeof Index.ListSuper).toEqual("function");
+  expect(typeof Index.PaginationSuper).toEqual("function");
+  expect(typeof Index.OrderUtils).toEqual("object");
+});
