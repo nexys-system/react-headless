322325915a5342d88027e7d46fe2a3ec9f7c4995
John B <johan@nexys.ch>
2022-08-07 13:52:34 +0200

    Create list-tree.tsx

---
+++ src/lib/list-tree.tsx
@@ -0,0 +1,23 @@
+const ListTree =
+  <A extends { children: A[] }>(
+    UL: (props: { children: JSX.Element[] }) => JSX.Element,
+    Line: (props: A) => JSX.Element
+  ) =>
+  ({ items }: { items: A[] }): JSX.Element => {
+    const LI = ListTree(UL, Line);
+
+    return (
+      <UL>
+        {items.map((x, i) => {
+          return (
+            <React.Fragment key={i}>
+              <Line {...x} />
+              {x.children && x.children.length > 0 && <LI items={x.children} />}
+            </React.Fragment>
+          );
+        })}
+      </UL>
+    );
+  };
+  
+  export default ListTree;
