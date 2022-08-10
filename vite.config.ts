e8b5a8188227373feda1e36a29e30b6b7570c7c2
Johan <johan@macair2020>
2022-08-10 11:38:40 +0200

    added conditional vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// this the default/base configuration
+const baseConfig = {
   plugins: [react()],
-  base: "/react-headless/",
   test: {
     // ...
   },
+};
+
+// https://vitejs.dev/config/
+// conditional config: https://vitejs.dev/config/#conditional-config
+export default defineConfig(({ mode }) => {
+  // console.log({ command, mode, ssrBuild });
+
+  // if  `mode===ghpages`: we add the path prefix to match the path prefix of gh pages
+  if (mode === "ghpages") {
+    return { ...baseConfig, base: "/react-headless/" };
+  }
+
+  return baseConfig;
 });
