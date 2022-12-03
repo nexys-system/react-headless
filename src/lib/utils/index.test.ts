b96f2168e2a4e7a4f950072110fceb7700e00561
John B <johan@nexys.ch>
2022-12-03 16:06:15 +0100

    Update index.test.ts

--- src/lib/utils/index.test.ts
+++ src/lib/utils/index.test.ts
@@ -30,18 +30,21 @@ test("parse JWT", () => {
 
 
 describe('getAnalyticsReadyPath', () => {
+  const prefixRegExp = new RegExp('^https://[a-z]+.mydomain.com')
+  
   test('without uuid', () => {
-    expect(U.getAnalyticsReadyPath('https://dev.mydomain.com/app')).toBe('/app');
-    expect(U.getAnalyticsReadyPath('https://test.mydomain.com/app')).toBe(
+    expect(U.getAnalyticsReadyPath('https://dev.mydomain.com/app'), prefixRegExp).toBe('/app');
+    expect(U.getAnalyticsReadyPath('https://test.mydomain.com/app'), prefixRegExp).toBe(
       '/app'
     );
-    expect(U.getAnalyticsReadyPath('https://app.mydomain.com/app')).toBe('/app');
+    expect(U.getAnalyticsReadyPath('https://app.mydomain.com/app'), prefixRegExp).toBe('/app');
   });
 
   test('with uuid', () => {
     expect(
       U.getAnalyticsReadyPath(
-        'https://dev.mydomain.com/app/9f9cbfb5-47b9-40e0-8q2d-0d3528a0d9ff/list'
+        'https://dev.mydomain.com/app/9f9cbfb5-47b9-40e0-8q2d-0d3528a0d9ff/list',
+        prefixRegExp
       )
     ).toBe('/app/:uuid/list');
   });
@@ -49,14 +52,17 @@ describe('getAnalyticsReadyPath', () => {
   test('with uuid (2x)', () => {
     expect(
       U.getAnalyticsReadyPath(
-        'https://dev.mydomain.com/app/9r9cbfb5-47a9-40e0-8e2d-6d3528a0d9ff/list/5d3f6d6a-f1ed-454d-8504-4yd4cf59c2a4/detail'
+        'https://dev.mydomain.com/app/9r9cbfb5-47a9-40e0-8e2d-6d3528a0d9ff/list/5d3f6d6a-f1ed-454d-8504-4yd4cf59c2a4/detail',
+        prefixRegExp
       )
     ).toBe('/app/:uuid/list/:uuid/detail');
   });
 
   test('wuth id', () => {
     expect(
-      U.getAnalyticsReadyPath('https://dev.mydomain.com/app/543/detail')
+      U.getAnalyticsReadyPath(
+        'https://dev.mydomain.com/app/543/detail'),
+        prefixRegExp
     ).toBe('/app/:id/detail');
   });
 });
