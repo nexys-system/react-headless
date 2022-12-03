e0654582115dda2f60d1a4bc620bedbaff6b9f2d
John B <johan@nexys.ch>
2022-12-03 15:13:31 +0000

    fix tests

--- src/lib/utils/index.test.ts
+++ src/lib/utils/index.test.ts
@@ -33,17 +33,17 @@ describe('getAnalyticsReadyPath', () => {
   const prefixRegExp = new RegExp('^https://[a-z]+.mydomain.com')
   
   test('without uuid', () => {
-    expect(U.getAnalyticsReadyPath('https://dev.mydomain.com/app'), prefixRegExp).toBe('/app');
-    expect(U.getAnalyticsReadyPath('https://test.mydomain.com/app'), prefixRegExp).toBe(
+    expect(U.getAnalyticsReadyPath('https://dev.mydomain.com/app', prefixRegExp)).toBe('/app');
+    expect(U.getAnalyticsReadyPath('https://test.mydomain.com/app', prefixRegExp)).toBe(
       '/app'
     );
-    expect(U.getAnalyticsReadyPath('https://app.mydomain.com/app'), prefixRegExp).toBe('/app');
+    expect(U.getAnalyticsReadyPath('https://app.mydomain.com/app', prefixRegExp)).toBe('/app');
   });
 
   test('with uuid', () => {
     expect(
       U.getAnalyticsReadyPath(
-        'https://dev.mydomain.com/app/9f9cbfb5-47b9-40e0-8q2d-0d3528a0d9ff/list',
+        'https://dev.mydomain.com/app/9f9cbfb5-47b9-40e0-8a2d-0d3528a0d9ff/list',
         prefixRegExp
       )
     ).toBe('/app/:uuid/list');
@@ -52,8 +52,8 @@ describe('getAnalyticsReadyPath', () => {
   test('with uuid (2x)', () => {
     expect(
       U.getAnalyticsReadyPath(
-        'https://dev.mydomain.com/app/9r9cbfb5-47a9-40e0-8e2d-6d3528a0d9ff/list/5d3f6d6a-f1ed-454d-8504-4yd4cf59c2a4/detail',
-        prefixRegExp
+        'https://dev.mydomain.com/app/9e9cbfb5-47a9-40e0-8e2d-6d3528a0d9ff/list/5d3f6d6a-f1ed-454d-8504-4dd4cf59c2a4/detail',
+        prefixRegExp 
       )
     ).toBe('/app/:uuid/list/:uuid/detail');
   });
@@ -61,8 +61,8 @@ describe('getAnalyticsReadyPath', () => {
   test('wuth id', () => {
     expect(
       U.getAnalyticsReadyPath(
-        'https://dev.mydomain.com/app/543/detail'),
-        prefixRegExp
+        'https://dev.mydomain.com/app/543/detail',
+        prefixRegExp)
     ).toBe('/app/:id/detail');
   });
 });
