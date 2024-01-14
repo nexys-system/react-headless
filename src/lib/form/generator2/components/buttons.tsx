2e2ab063b141f5fce6dcb1ed65bab87913ee796e
Johan <johan@macair2020>
2024-01-14 10:52:43 +0100

    build works

--- src/lib/form/generator2/components/buttons.tsx
+++ src/lib/form/generator2/components/buttons.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { SubmitButtonProps } from "../../type";
 import Spinner from "./spinner";
 
--- src/lib/form/generator2/components/inputs.tsx
+++ src/lib/form/generator2/components/inputs.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   InputOptionProps,
   InputProps,
